235203e84f9f18e489895b8353fe14511e04e2e0
DANISH ZULFIQAR <rana.noman79773@gmail.com>
2026-07-08 08:40:28 +0500

    Update editor.py

--- editor.py
+++ editor.py
@@ -1,21 +1,19 @@
 from gradio_client import Client, handle_file
 from PIL import Image
-from io import BytesIO
 import tempfile
 
-# Replace this with YOUR Space name
-SPACE_NAME ="Rd786/DanishZulfiqar"
+SPACE_NAME = "Rd786/DanishZulfiqar"
 
-client = Client("Rd786/DanishZulfiqar")
+def edit_image(image, prompt, api_key=""):
+    client = Client(SPACE_NAME)
 
-def edit_image(image: Image.Image, prompt: str, api_key: str = "") -> Image.Image:
     with tempfile.NamedTemporaryFile(suffix=".png", delete=False) as tmp:
         image.save(tmp.name)
 
         result = client.predict(
             image=handle_file(tmp.name),
             prompt=prompt,
-            api_name="/predict"   # This depends on the Space
+            api_name="/predict",
         )
 
     return Image.open(result).convert("RGB")
